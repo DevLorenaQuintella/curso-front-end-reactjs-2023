1a56bf2b6159de1d792e0287ce1e9bae287f33c9
Lorena <130595604+DevLorenaQuintella@users.noreply.github.com>
2023-09-27 18:43:38 +0000

    itens adicionados

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,36 @@
 
 import './App.css'
+import Card from './components/Card'
 
 function App() {
-  
+  const item1 = {
+    nome: 'Naruto',
+    imagemUrl:'https://tm.ibxk.com.br/2021/10/05/05091846887061.jpg'
+  }
+  const item2 = {
+    nome: 'One Piece',
+    imagemUrl: 'https://tm.ibxk.com.br/2022/11/03/03095855664065.jpg'
+  }
+  const item3 = {
+    nome: 'Demon Slayer',
+    imagemUrl: 'https://tm.ibxk.com.br/2021/04/15/15130752762166.jpg'
+  }
+  const item4 = {
+    nome: 'Hunter x Hunter',
+    imagemUrl: 'https://tm.ibxk.com.br/2018/11/12/12084324945033.jpg'
+  }
+  const item5 = {
+    nome: 'Jujutsu Kaisen',
+    imagemUrl:'https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2023%2F08%2Fjujutsu-kaisen-fortnite-collaboration-teaser-info-00.jpg?w=960&cbr=1&q=90&fit=max'
+  }
+  const itens = [item1, item2, item3, item4, item5]
   return (
     <>
-      <div className='Card'>
-       <h2>Naruto</h2>
-
-       <div className='tags'>
-        <div className='tag'>Status: Anime</div>
-        <div className='tag'>História: Naruto Uzumaki</div>
-        <div className='tag'>Escrita: Masashi Kishimoto</div>      
-       </div>
-       <img src="https://tm.ibxk.com.br/2021/10/05/05091846887061.jpg" />
-      </div>
-
-      <div className='Card'>
-       <h2>One Piece</h2>
-
-       <div className='tags'>
-        <div className='tag'>Status: Anime</div>
-       <div className='tag'>História: Monkey D. Luffy</div>
-        <div className='tag'>Escrita: Eiichiro Oda</div>
-        </div>
-       <img src="https://tm.ibxk.com.br/2022/11/03/03095855664065.jpg" />
-      </div>
-
-      <div className='Card'>
-       <h2>Demon Slayer</h2>
-
-       <div className='tags'>
-        <div className='tag'>Status: Anime</div>
-       <div className='tag'>História: Matador de Demônios</div>
-        <div className='tag'>Escrita: Koyoharu Gotōge</div>
-        </div>
-       <img src="https://tm.ibxk.com.br/2021/04/15/15130752762166.jpg" />
-      </div>
-      
+    {itens.map(function (item) {
+      return <Card item={item}/>
+    })}
+    
+    
     </>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import "./Card.css"
+
+export default function Card (props) {
+    const item = props.item;
+
+    return <div className='Card'>
+    <h2>{item.nome}</h2>
+
+    <div className='tags'>
+     <div className='tag'>Status: Anime</div>
+     <div className='tag'>Espécie: Humana</div>
+     <div className='tag'>Origem: Japão</div>      
+    </div>
+    <img src={item.imagemUrl} />
+   </div>
+
+
+} 
