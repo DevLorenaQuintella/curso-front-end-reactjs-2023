3316cb464068b9c3484327241ade9105c7ee3b65
Lorena <130595604+DevLorenaQuintella@users.noreply.github.com>
2023-09-27 16:43:21 +0000

    criação e estilização das tags

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,34 @@ function App() {
     <>
       <div className='Card'>
        <h2>Naruto</h2>
+
+       <div className='tags'>
+        <div className='tag'>Status: Anime</div>
+        <div className='tag'>História: Naruto Uzumaki</div>
+        <div className='tag'>Escrita: Masashi Kishimoto</div>      
+       </div>
        <img src="https://tm.ibxk.com.br/2021/10/05/05091846887061.jpg" />
       </div>
 
       <div className='Card'>
        <h2>One Piece</h2>
+
+       <div className='tags'>
+        <div className='tag'>Status: Anime</div>
+       <div className='tag'>História: Monkey D. Luffy</div>
+        <div className='tag'>Escrita: Eiichiro Oda</div>
+        </div>
        <img src="https://tm.ibxk.com.br/2022/11/03/03095855664065.jpg" />
       </div>
-      
+
       <div className='Card'>
        <h2>Demon Slayer</h2>
+
+       <div className='tags'>
+        <div className='tag'>Status: Anime</div>
+       <div className='tag'>História: Matador de Demônios</div>
+        <div className='tag'>Escrita: Koyoharu Gotōge</div>
+        </div>
        <img src="https://tm.ibxk.com.br/2021/04/15/15130752762166.jpg" />
       </div>
       
